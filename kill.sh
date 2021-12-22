d53d09728c6d5ad1c624e003a11fcc6f32489a90
mowenxun <539996850@qq.com>
2021-12-22 17:03:18 +0800

    先删后重启

--- kill.sh
+++ kill.sh
@@ -1,2 +1,9 @@
 # 先删除半小时轮询检查all.log大小的进程
-kill -9 $(ps -ef|grep ry.sh|grep -v grep|awk '{print $2}')
+PID='ps -ef|grep ry.sh|grep -v grep|awk '{print $2}''
+if [ x"$PID" != x"" ]; then
+	    echo "ry.sh is running...,kill thread"
+	    kill -TERM $PID
+	    echo "kill thread success"
+	else
+	   echo "ry.sh is notRunning..."
+	fi
