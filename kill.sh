152e876244555d02809ce0b884e83862b2aa75fc
mowenxun <539996850@qq.com>
2022-02-22 11:50:36 +0800

    优化

--- kill.sh
+++ kill.sh
@@ -1,9 +1,9 @@
 # 先删除半小时轮询检查all.log大小的进程
-PID=`ps -ef|grep ry.sh|grep -v grep|awk '{print $2}'`
+PID=`ps -ef|grep ry-8090.sh|grep -v grep|awk '{print $2}'`
 if [ x"$PID" != x"" ]; then
-	    echo "ry.sh is running...,kill thread"
+	    echo "ry-8090.sh is running...,kill thread"
 	    kill -TERM $PID
 	    echo "kill thread success"
 	else
-	   echo "ry.sh is notRunning..."
+	   echo "ry-8090.sh is notRunning..."
 	fi
